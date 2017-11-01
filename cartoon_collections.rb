9fffdade89d81db2485f238a54c16785e87fca15
Blakeley Bond <burrick2003@yahoo.com>
2017-11-01 18:43:22 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -32,7 +32,7 @@ def find_the_cheese(input_array)# code an argument here
   counter = 0
     while counter < cheese_types.size do
       if input_array.include?(cheese_types[counter])
-        output = "#{input.array[counter]}"
+        output = "#{input_array[counter]}"
       end
       counter += 1
     end
